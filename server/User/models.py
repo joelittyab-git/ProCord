fd376ab34a17d074fe6ef93ee19834ba2b3bcae9
joelittyab-git <joeittab@gmail.com>
2023-08-23 12:16:08 +0530

    added profile models for users and friednship through model

--- server/User/models.py
+++ server/User/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''
+------------------------------------------------------------------------------Friendship-model[through]-------------------------------------------------------------------------------------------------
+[user]:OneToOneField -> points to the user of a particular profile
+[friend_list]:ManyToManyField -> points to the user(:User) of the friendship (Many Friendship -> single User)
+-------------------------------------------------------------------------------------------------------------------------------------------
+'''
+class Profile(models.Model):
+     user = models.OneToOneField(
+          User,
+          on_delete=models.CASCADE
+     )
+     
+     friend_list = models.ManyToManyField("self", through="Friendship")
+     
+     
+'''
+------------------------------------------------------------------------------Friendship-model[through]-------------------------------------------------------------------------------------------------
+[friendship_id]:IntegerField -> stores the primary key of the particular friendship
+[user1]:ForeignKey -> points to the user(:User) of the friendship (Many Friendship -> single User)
+[user2]:ForeignKey -> points to the user(:User) of the friendship (Many Friendship -> single User)
+[created_at]:DateTimeField -> stores the date and time of the friendship created
+[category]:CharField -> stores the type of friendship
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+class Friendship(models.Model):
+     
+     friendship_id = models.BigAutoField(primary_key=True)
+     
+     user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_a")
+     user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_b")
+     
+     created_at = models.DateTimeField(auto_now_add=True)
+     category = models.CharField(max_length=100)
+     
+     
