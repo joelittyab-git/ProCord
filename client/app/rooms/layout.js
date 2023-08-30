5f4a44cbbd04dba618ad89403a0c4d5b5f1133d2
joelittyab-git <joeittab@gmail.com>
2023-08-30 18:34:38 +0530

    edited SideBar component

--- client/app/rooms/layout.js
+++ client/app/rooms/layout.js
@@ -3,9 +3,9 @@ import SideBar from '@/components/SideBar';
 
 function layout({children}) {
   return (
-    <main>
+    <main className=''>
      <div className='flex'>
-          <div className='sidebar absolute h-screen w-10 sm:w-14 md:w-17 lg:w-14 xl:w-16 2xl:w-17 items-center  transition-all duration-300 hover:w-60'>
+          <div className='sidebar absolute h-screen items-center'>
                <SideBar/>
           </div>
           <div className=' bg-white w-screen h-screen'>{children}</div>
--- client/components/SideBar.js
+++ client/components/SideBar.js
@@ -8,31 +8,34 @@ import Divider from '@mui/material/Divider';
 
 function SideBar() {
   return (
-    <div className=' bg-black h-screen flex align-middle items-center  w-10 sm:w-14 md:w-17 lg:w-14 xl:w-16 2xl:w-17 transition-all duration-300 hover:w-60 group'>
+    <div className=' bg-black h-screen flex align-middle items-center  w-10 sm:w-14 md:w-17 lg:w-14 xl:w-16 2xl:w-17 overflow-hidden group transition-all duration-700 hover:w-64 z-50'>
      <div className=' w-full pb-10 space-y-6'>
-          <div className='flex items-center justify-center '>
+          <div className='flex items-center justify-center group/group1 transition-colors duration-300 hover:bg-blue-600' >
                <IconButton color='white'>
-                    <ExitToAppIcon htmlColor='#c2c2c2' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl hover:text-white'/>
+                    <ExitToAppIcon htmlColor='#c2c2c2' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl group-hover/group1:text-white transition duration-700 group-hover:-translate-x-10'/>
+                    <p class="hidden absolute top-0 left-8 text-lg p-2 text-slate-300 group-hover:block transition duration-300 ease-in-out font-semibold ">Exit</p>
                </IconButton>
           </div>
      
-          <div className='flex justify-center items-center'>
+          <div className='flex justify-center items-center group/group2  transition-colors duration-300 hover:bg-blue-600'>
                <IconButton color='white'>
-                    <GroupIcon htmlColor='#c2c2c2' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl hover:text-white '/>
-                    <p class="transition hidden  group-hover:block  text-white  text-lg  pl-2  ">Group</p>
+                    <GroupIcon htmlColor='#c2c2c2' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl group-hover/group2:text-white transition duration-700 group-hover:-translate-x-10 justify-center'/>
+                    <p class="hidden absolute top-0 left-8  text-lg p-2 text-slate-300 group-hover:block transition duration-300 ease-in-out font-semibold">Group</p>
                </IconButton>
           </div>
      
-          <div className='flex justify-center items-center'>
+          <div className='flex justify-center items-center group/group3  transition-colors duration-300 hover:bg-blue-600'>
                <IconButton color='white'>
-                    <PersonIcon htmlColor='#c2c2c2' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl  hover:text-white '/>
+                    <PersonIcon htmlColor='#c2c2c2' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl group-hover/group3:text-white transition duration-700 group-hover:-translate-x-10'/>
+                    <p class="hidden absolute top-0 left-8 text-lg p-2 text-slate-300 group-hover:block transition duration-300 ease-in-out font-semibold">Messages</p>
                </IconButton>
           </div>
 
-          <div className="absolute bottom-3 justify-center w-full">
-               <div className='flex justify-center items-end pt-6 '>
+          <div className="absolute bottom-3 justify-center w-full overflow-hidden items-center">
+               <div className='flex justify-center items-center   transition-colors duration-300 hover:bg-blue-600'>
                     <IconButton color='white'>
-                         <AccountCircleIcon htmlColor='white' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl  hover:text-white  '/>
+                         <AccountCircleIcon htmlColor='white' className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl group-hover/group2:text-white transition duration-700 group-hover:-translate-x-10'/>
+                         <p class="hidden absolute top-0 left-8 text-lg p-2 text-slate-300 group-hover:block transition duration-300 ease-in-out font-semibold">Account</p>
                     </IconButton>
                </div>
           </div>
