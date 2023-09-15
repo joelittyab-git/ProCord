7a3c8d92060751c883f55f95a539754749133c02
joelittyab-git <joeittab@gmail.com>
2023-09-15 23:02:09 +0530

    views bug fix

--- server/ChatRoom/views.py
+++ server/ChatRoom/views.py
@@ -1,19 +1,16 @@
-from django.shortcuts import render
-
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.http import HttpRequest
 from django.contrib.auth.models import User
-from .models import(
-     Room,
-     RoomMembership
+from .models import (
+    Room,
+    RoomMembership
 )
 from .serializers import (
-     RoomMembershipModelSerializer,
-     RoomModelSerializer,
-     MessageModelSerializer
+    RoomMembershipModelSerializer,
+    RoomModelSerializer,
+    MessageModelSerializer
 )
 
 '''
@@ -37,75 +34,78 @@ from .serializers import (
           {"status":"exception","info":{...}}} -> database integrity error / other exceptions
           
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-''' 
+'''
+
+
 class RoomRegistrationView(APIView):
-     permission_classes = [
-          IsAuthenticated,
-     ]
-     
-     def post(self, request:HttpRequest, *args, **kwargs):  
-          # extracting request data        
-          admin = request.user
-          room_name = request.data.get("title")
-          room_description = request.data.get("description")
-          participant_list = request.data.get("users")
-          
-          # attemting to create a room entry to the database
-          try:
-               room = Room.objects.create(
-                    name = room_name,
-                    description = room_description,
-                    admin = admin,
-               )
-               room.save()
-          except Exception as e:
-               return Response({"status":"exception", "info":str(e)})
-          
-          # extracting participant list from data and adding them
-          for username in participant_list:
-               try: users = User.objects.get(username = username)
-               except Exception as e:return Response({"status":"exception", "info":str(e)})
-               
-               RoomMembership.objects.create(
-                    room = room,
-                    user = users
-               ).save()
+    permission_classes = [
+        IsAuthenticated,
+    ]
 
-          return Response({"status":"success"})
-          
-     def get(self, request:HttpRequest, *args, **kwargs):
-          user = request.user
-          
-          # filtering from datbase, the rooms for which the logged in user is an admin of
-          rooms_admin = Room.objects.filter(admin = user)
-          serialized_a = RoomModelSerializer(
-               rooms_admin,
-               many = True
-          )
-          
-          # filtering from datbase, the rooms for whcih the logged in user is a participant of
-          rooms = RoomMembership.objects.filter(
-               user = user
-          )
-          serialized_b = RoomMembershipModelSerializer(rooms, many = True)
-          
-          room = []
-          
-          # getting the room for each membership
-          try:
-               for members in serialized_b.data:
-                    member = dict(members)
-                    member_room = Room.objects.get(room_id=(member.get("room")))
-                    room_serialized =  RoomModelSerializer(member_room)
-                    room+=[room_serialized.data]
-          except Exception as e:return Response({"status":"exception", "info":{str(e)}})
-                
-          room = room + (serialized_a.data)
+    def post(self, request: HttpRequest, *args, **kwargs):
+        # extracting request data
+        admin = request.user
+        room_name = request.data.get("title")
+        room_description = request.data.get("description")
+        participant_list = request.data.get("users")
 
-          # rooms = Room.objects.filter(admin=user)
-          return Response({"status":"success", "rooms":room})
+        # attemting to create a room entry to the database
+        try:
+            room = Room.objects.create(
+                name=room_name,
+                description=room_description,
+                admin=admin,
+            )
+            room.save()
+        except Exception as e:
+            return Response({"status": "exception", "info": str(e)})
 
+        # extracting participant list from data and adding them
+        for username in participant_list:
+            try:
+                users = User.objects.get(username=username)
+            except Exception as e:
+                return Response({"status": "exception", "info": str(e)})
 
+            RoomMembership.objects.create(
+                room=room,
+                user=users
+            ).save()
+
+        return Response({"status": "success"})
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        user = request.user
+
+        # filtering from datbase, the rooms for which the logged in user is an admin of
+        rooms_admin = Room.objects.filter(admin=user)
+        serialized_a = RoomModelSerializer(
+            rooms_admin,
+            many=True
+        )
+
+        # filtering from datbase, the rooms for whcih the logged in user is a participant of
+        rooms = RoomMembership.objects.filter(
+            user=user
+        )
+        serialized_b = RoomMembershipModelSerializer(rooms, many=True)
+
+        room = []
+
+        # getting the room for each membership
+        try:
+            for members in serialized_b.data:
+                member = dict(members)
+                member_room = Room.objects.get(room_id=(member.get("room")))
+                room_serialized = RoomModelSerializer(member_room)
+                room += [room_serialized.data]
+        except Exception as e:
+            return Response({"status": "exception", "info": {str(e)}})
+
+        room = room + serialized_a.data
+
+        # rooms = Room.objects.filter(admin=user)
+        return Response({"status": "success", "rooms": room})
 
 
 '''
@@ -122,35 +122,34 @@ class RoomRegistrationView(APIView):
           {"action":"redirect"} -> page should be redirected 
           {"action":"exception","info":{...}}} -> database integrity error / other exceptions
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-'''  
+'''
+
+
 class RoomManagerView(APIView):
-     permission_classes = [
-          IsAuthenticated
-     ]
-     
-     def get(self, request:HttpRequest, room_id, *args, **kwargs):
-          
-          # extracting request data
-          user = request.user
-          room = Room.objects.filter(room_id=room_id)
-          
-          # checking if the room with this number exists
-          if(not room.exists() ) :
-               return Response({"action":"redirect"})
-          
-          room = Room.objects.get(room_id=room_id)
-          membership = RoomMembership.objects.filter(user=user, room__room_id = room_id)
-          if(
-               not ((room.admin.pk == user.pk) or 
-               (membership.exists()))
-          ):
-               return Response({"action":"redirect"})
-          
-          # querying messages from the room model and serializing it
-          messages = room.messages
-          message_serialized = MessageModelSerializer(messages, many = True)
-          
-          return Response({"action":"display", "messages":message_serialized.data})
-          
-     
-     
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def get(self, request: HttpRequest, room_id, *args, **kwargs):
+
+        # extracting request data
+        user = request.user
+        room = Room.objects.filter(room_id=room_id)
+
+        # checking if the room with this number exists
+        if not room.exists():
+            return Response({"action": "redirect"})
+
+        room = Room.objects.get(room_id=room_id)
+        membership = RoomMembership.objects.filter(user=user, room__room_id=room_id)
+        if (
+                not ((room.admin.pk == user.pk) or
+                     (membership.exists()))
+        ):
+            return Response({"action": "redirect"})
+
+        # querying messages from the room model and serializing it
+        messages = room.messages
+        message_serialized = MessageModelSerializer(messages, many=True)
+
+        return Response({"action": "display", "messages": message_serialized.data})
